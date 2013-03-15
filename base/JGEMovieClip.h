d66bd6c5031f122e44e5eb22d73a0287e098cbcd
chengkehan <chengkehan@gamil.com>
2013-03-15 22:28:41 +0800

    movieclip temp

---
+++ base/JGEMovieClip.h
@@ -0,0 +1,43 @@
+#ifndef __JGE_MOVIECLIP_H__
+#define __JGE_MOVIECLIP_H__
+
+#include "JGEDisplayObject.h"
+
+class JGEMovieClipFrame
+{
+public:
+	JGEMovieClipFrame();
+	~JGEMovieClipFrame();
+
+	JGETexture* m_lpTexture;
+	float m_refX;
+	float m_refY;
+};
+
+class JGEMovieClip : public JGEDisplayObject
+{
+public:
+	JGEMovieClip(IDirect3DDevice9* lpd3dd);
+	~JGEMovieClip();
+
+	void setFrames(const JGEMovieClipFrame* lpFrames, uint numFrames);
+	inline const JGEMovieClipFrame* getFrames() const { return m_lpFrames; }
+	inline int getNumFrames() const { return m_numFrames; }
+	bool gotoFrame(int frameIndex);
+	inline int getCurrentFrameIndex() const { return m_frameIndex; }
+	inline void setLoopFrame(bool loop) { m_loopFrame = loop; }
+	inline bool getLoopFrame() const { return m_loopFrame; }
+
+private:
+	JGEMovieClip();
+	JGEMovieClip(const JGEMovieClip& value);
+
+	JGEMovieClipFrame* m_lpFrames;
+	int m_numFrames;
+	int m_frameIndex;
+	bool m_loopFrame;
+
+	void destroyFrames();
+};
+
+#endif
