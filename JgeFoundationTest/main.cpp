1a8738c5e95e16bc288c8e559f929fb5df9fed81
chengkehan <chengkehan@gmail.com>
2013-08-18 00:33:42 +0800

    history node

---
+++ JgeFoundationTest/main.cpp
@@ -0,0 +1,11 @@
+#include "JgeStdafx.h"
+#include "JgeMath.h"
+
+#pragma comment(lib, "JgeFoundation.lib")
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevHInstance, LPSTR lpCmdLine, INT nCmdShow)
+{
+	jge::Rect rect;
+	jge::Matrix4x4 matrix;
+	return 0;
+}
