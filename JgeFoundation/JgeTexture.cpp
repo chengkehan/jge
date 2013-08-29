27ae6753beee2cf8b77c5685f83693a4ecd3de07
chengkehan <chengkehan@gmail.com>
2013-08-30 00:16:17 +0800

    history node

--- JgeFoundation/JgeTexture.cpp
+++ JgeFoundation/JgeTexture.cpp
@@ -1,7 +1,27 @@
 #include "JgeStdafx.h"
 #include "JgeTexture.h"
 
-jge::Texture2D::Texture2D()
+jge::Texture2D::Texture2D(Jge3D* lpJge3D):
+	m_filter(jge::TextureFilter::TEXTURE_FILTER_NONE), m_width(0), m_height(0), m_lpJge3D(lpJge3D)
 {
+	// Do nothing
+}
 
+jge::Texture2D::Texture2D(Jge3D* lpJge3D, uint width, uint height, TextureFilter filter):
+	m_lpJge3D(lpJge3D), m_width(width), m_height(height), m_filter(filter)
+{
+	// Create Texture
+}
+
+jge::Texture2D::~Texture2D()
+{
+	release();
+}
+
+void jge::Texture2D::release()
+{
+	m_filter = jge::TextureFilter::TEXTURE_FILTER_NONE;
+	m_width = 0;
+	m_height = 0;
+	m_lpJge3D = null;
 }
--- JgeFoundation/JgeTexture.h
+++ JgeFoundation/JgeTexture.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <d3d9.h>
-#include <d3dx9.h>
 #include "JgeCommon.h"
+#include "Jge3D.h"
 
 namespace jge
 {
@@ -17,14 +16,24 @@ namespace jge
 	class JGE_DLL Texture2D
 	{
 	public:
-		Texture2D();
-		Texture2D(uint width, uint height, TextureFilter filter);
+		Texture2D(Jge3D* lpJge3D);
+		Texture2D(Jge3D* lpJge3D, uint width, uint height, TextureFilter filter);
 		~Texture2D();
 		
+		inline TextureFilter getFilter() { return m_filter; }
+		inline uint getWidth() { return m_width; }
+		inline uint getHeight() { return m_height; }
+
+
 	private:
 		Texture2D(const Texture2D& value);
 		Texture2D& operator=(const Texture2D& value);
 
 		TextureFilter m_filter;
+		uint m_width;
+		uint m_height;
+		Jge3D* m_lpJge3D;
+
+		void release();
 	};
 }
